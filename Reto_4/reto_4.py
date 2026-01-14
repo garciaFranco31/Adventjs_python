7502a953d88276f8209c2ca5289bc985810c6492
Franco Garcia <garciacfranco2001@gmail.com>
2026-01-13 21:03:38 -0300

    reto 4 finalizado

--- Reto_4/reto_4.py
+++ Reto_4/reto_4.py
@@ -1,22 +1,27 @@
 code = "[1++][2-][3+][<]"
 
-digits = []
-parts = code.strip("[]").split("][")
+def obtener_codigo(codigo):
+    digits = []
+    parts = code.strip("[]").split("][")
 
-print(parts)
-for part in parts:
-    if (part == "<"):
-        if digits:
-            digits.append(digits[-1])
+    for part in parts:
+        if (part == "<"):
+            if digits:
+                digits.append(digits[-1])
+            else:
+                print("no hay nada")
         else:
-            print("no hay nada")
+            num = int(part[0])
+            for op in part[1:]:
+                if op == "+":
+                    num = (num + 1) % 10
+                elif (op == "-"):
+                    num = (num-1) % 10
+            digits.append(num)
+    if (len(digits) >= 4):
+        #print(digits)
+        return ''.join(map(str,digits))
     else:
-        num = int(part[0])
-        for op in part[1:]:
-            if op == "+":
-                num = (num + 1) % 10
-            elif (op == "-"):
-                num = (num-1) % 10
-        digits.append(num)
-if (len(digits) >= 4):
-    print(digits)
+        return None
+
+print(obtener_codigo(code))
