22886c4844fc0e37e699a79a8899486b2d77783a
Franco Garcia <garciacfranco2001@gmail.com>
2026-01-27 20:31:11 -0300

    reto 5 iniciado

---
+++ Reto_5/reto_5.py
@@ -0,0 +1,36 @@
+"""En el taller de Santa, los elfos han encontrado una montaña de guantes mágicos totalmente desordenados. Cada guante viene descrito por dos valores:
+
+hand: indica si es un guante izquierdo (L) o derecho (R)
+color: el color del guante (string)
+Tu tarea es ayudarles a emparejar guantes: Un par válido es un guante izquierdo y uno derecho del mismo color.
+
+🧩 Ejemplos
+const gloves = [
+  { hand: 'L', color: 'red' },
+  { hand: 'R', color: 'red' },
+  { hand: 'R', color: 'green' },
+  { hand: 'L', color: 'blue' },
+  { hand: 'L', color: 'green' }
+]
+
+matchGloves(gloves)
+// ["red", "green"]
+
+"""
+
+
+from typing import List, Dict
+
+def match_gloves(gloves: List[Dict[str, str]]) -> List[str]:
+    for elem in gloves:
+        print(elem)
+
+gloves = [
+  { "hand": 'L', "color": 'red' },
+  { "hand": 'R', "color": 'red' },
+  { "hand": 'R', "color": 'green' },
+  { "hand": 'L', "color": 'blue' },
+  { "hand": 'L', "color": 'green' }
+]
+
+match_gloves(gloves)
